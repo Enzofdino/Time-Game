99aed9775a123da698c0b866b59f9463bb1621ec
EduardoAdB <eduardo.bruns@alunos.sc.senac.br>
2024-07-04 08:47:21 -0300

    Spawn delete feito

--- Batata/Assets/script/SpawnManager.cs
+++ Batata/Assets/script/SpawnManager.cs
@@ -19,6 +19,7 @@ public class SpawnManager : MonoBehaviour
     bool stMed;
     bool stCont;
     bool stMod;
+
     GameObject structure;
 
     int stuPrim;
@@ -30,6 +31,11 @@ public class SpawnManager : MonoBehaviour
     int contMed;
     int contMod;
     int contCont;
+
+    private GameObject[] spawnedPrimStructures;
+    private GameObject[] spawnedMedStructures;
+    private GameObject[] spawnedContStructures;
+    private GameObject[] spawnedModStructures;
 
     private void Start()
     {
@@ -47,54 +53,114 @@ public class SpawnManager : MonoBehaviour
         stuMed = Random.Range(4, 10);
         stuCont = Random.Range(4, 10);
         stuMod = Random.Range(4, 10);
+
+        spawnedPrimStructures = new GameObject[stuPrim];
+        spawnedMedStructures = new GameObject[stuMed];
+        spawnedContStructures = new GameObject[stuCont];
+        spawnedModStructures = new GameObject[stuMod];
     }
     private void Update()
     {
-        if ((Contador.instance.era == "PréHistórica") && (stPrim == true)&&(contPrim != stuPrim))
+        if ((Contador.instance.era == "PréHistórica") && (stPrim == true) && (contPrim != stuPrim))
         {
 
-                do
-                {
-                    structure = Instantiate(structurePrim[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
-
-                    contPrim = contPrim + 1;
-                }
-                while (contPrim != stuPrim);
+            do
+            {
+                structure = Instantiate(structurePrim[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+                spawnedPrimStructures[contPrim] = structure;
+                contPrim++;
+            }
+            while (contPrim != stuPrim);
             stMed = true;
+            stPrim = false;
 
 
         }
-        if ((Contador.instance.era == "Medieval")&& (stMed == true))
+        if ((Contador.instance.era == "Medieval") && (stMed == true) && (contMed != stuMed))
         {
-            Destroy(structure);
-            structure = Instantiate(structureMed[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+            DestroyAllPrimStructures();
+            do
+            {
+                structure = Instantiate(structureMed[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+                spawnedMedStructures[contMed] = structure;
+                contMed++;
+            }
+            while (contMed != stuMed);
             stMed = false;
             stCont = true;
         }
-        if ((Contador.instance.era == "Contemporânea")&&(stCont == true))
+        if ((Contador.instance.era == "Contemporânea") && (stCont == true) && (contCont != stuCont))
         {
-            Destroy(structure);
-            structure = Instantiate(structureCont[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+            DestroyAllMedStructures();
+            do
+            {
+                structure = Instantiate(structureCont[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+                spawnedContStructures[contCont] = structure;
+                contCont++;
+            }
+            while (contCont != stuCont);
             stCont = false;
             stMod = true;
         }
-        if ((Contador.instance.era== "Moderna")&&(stMod == true))
+        if ((Contador.instance.era == "Moderna") && (stMod == true) && (contMod != stuMod))
         {
-            Destroy(structure);
-            structure = Instantiate(structureMod[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+            DestroyAllContStructures();
+            do
+            {
+                structure = Instantiate(structureMod[0], new Vector3(Random.Range(31, 104), 0f, 0f), Quaternion.identity);
+                spawnedModStructures[contMod] = structure;
+                contMod++;
+            }
+            while (contMod != stuMod);
             stMod = false;
         }
-
-
-
-
-
     }
+        private void DestroyAllPrimStructures()
+        {
+            for (int i = 0; i < contPrim; i++)
+            {
+                Destroy(spawnedPrimStructures[i]);
+            }
+            contPrim = 0;
+        }
+
+        private void DestroyAllMedStructures()
+        {
+            for (int i = 0; i < contMed; i++)
+            {
+                Destroy(spawnedMedStructures[i]);
+            }
+            contMed = 0;
+        }
+
+        private void DestroyAllContStructures()
+        {
+            for (int i = 0; i < contCont; i++)
+            {
+                Destroy(spawnedContStructures[i]);
+            }
+            contCont = 0;
+        }
+
+        private void DestroyAllModStructures()
+        {
+            for (int i = 0; i < contMod; i++)
+            {
+                Destroy(spawnedModStructures[i]);
+            }
+            contMod = 0;
+        }
+    }       
+
+
+
+
+
 
     //identificar cada prefab para cada era (FEITO)
 
 
-    //definir uma área para gerar o prefab  (A FAZER)
+    //definir uma área para gerar o prefab  (Feito)
 
 
     //instanciar o prefab, dentro da área definida, apenas se não houver outro prefab na área (A FAZER)
@@ -110,4 +176,4 @@ public class SpawnManager : MonoBehaviour
 
 
     //ao mudar de era, substituir os prefabs pelos prefabs da era seguinte (FEITO)
-}
+

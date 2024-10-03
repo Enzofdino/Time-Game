0a58d2c8a67cb4d1aed7f5fb3d063d7c861f3a92
Enzofdino <enzofhostin@gmail.com>
2024-10-03 14:16:30 -0300

    commit meu
    começo do script porta talvez terá alterações

--- Batata/Assets/script/Portas.cs
+++ Batata/Assets/script/Portas.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Portas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool portafechada;
+    GameObject portaaberta;
+    bool question1;
+    bool questioncerta;
+    GameObject jogador;
+
+   
+
+    public void OnCollisionEnter2D(Collision2D collision, GameObject porta)
     {
+        if (jogador = porta)
+        {
+            collision.gameObject.SetActive(true);
+            Debug.Log("Jogador colidiu com a porta");
+        }
+    }
+    public void Interagirporta(bool acertou)
+    {
+        
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            portaaberta.SetActive(true);
+        }
+        
+    }
+    
+    public void Perguntas(TextMeshProUGUI proximaquestion)
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            portafechada = true;
+            if(question1 == true)
+            {
+                proximaquestion.ToString();            
+            }
+            else if(questioncerta == false)
+            {
+                question1 = true;
+            }
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
